a147dc0a6d845b383154873b1354f61f2cec543d
Tatyana Izmailova <rutaizm@yandex.ru>
2022-02-21 11:19:25 +0300

    added profile info in form

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ let closeBtn = document.querySelector('.pop-up__close');
 let formElement = document.querySelector('.edit-form');
 let nameInput = formElement.querySelector('.edit-form__name');
 let jobInput = formElement.querySelector('.edit-form__info');
-
+let names = document.querySelector('.profile__name');
+let job = document.querySelector('.profile__info');
 
 function openPopup() {
     popup.classList.remove("pop-up__hidden");
-
-    let name = document.querySelector('.profile__name');
-    let job = document.querySelector('.profile__info');
-
-    name.textContent = nameInput.value;
-    job.textContent = jobInput.value;
+     nameInput.value = names.textContent;
+     jobInput.value = job.textContent;
 }
 
 function closePopup () {
