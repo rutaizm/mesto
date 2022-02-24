0932fe4bf2129e46a8d9b55136adfeb96a3dfe69
Tatyana Izmailova <rutaizm@yandex.ru>
2022-02-24 13:11:07 +0300

    pop-up fixed

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,19 @@ let popup = document.querySelector('.pop-up');
 let editProfile = document.querySelector('.profile__edit-button');
 let closeBtn = document.querySelector('.pop-up__close');
 let formElement = document.querySelector('.edit-form');
-let nameInput = formElement.querySelector('.edit-form__name');
-let jobInput = formElement.querySelector('.edit-form__info');
+let nameInput = formElement.querySelector('.edit-form_js-name');
+let jobInput = formElement.querySelector('.edit-form_js-info');
 let names = document.querySelector('.profile__name');
 let job = document.querySelector('.profile__info');
 
 function openPopup() {
-    popup.classList.remove("hidden");
+    popup.classList.add("pop-up_opened");
     nameInput.value = names.textContent;
     jobInput.value = job.textContent;  
 }
 
 function closePopup () {
-    popup.classList.add("hidden");
+    popup.classList.remove("pop-up_opened");
 }
 
 editProfile.addEventListener("click", openPopup);
@@ -26,5 +26,6 @@ function formSubmitHandler (evt) {
     jobInput = jobInput.value;
     names.textContent = nameInput;
     job.textContent = jobInput;
+    closePopup ();
 }
 formElement.addEventListener('submit', formSubmitHandler); 
