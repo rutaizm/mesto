9b02ced3480a6c613570e9cdf9ce1fb8f9f42cc5
Tatyana Izmailova <rutaizm@yandex.ru>
2022-03-28 12:25:40 +0300

    validation added

--- scripts/index.js
+++ scripts/index.js
@@ -172,26 +172,27 @@ function showBigPhoto(image, title) {
 
 // Валидация
 
-//
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, config) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.add('edit-form__field_type_error');
+    inputElement.classList.add(config.inputErrorClass);
     errorElement.textContent = errorMessage;
-    errorElement.classList.add('edit-form__input-error_active');
+    errorElement.classList.add(config.errorClass);
 };
 
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, config) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.remove('edit-form__field_type_error');
-    errorElement.classList.remove('edit-form__input-error_active');
+    inputElement.classList.remove(config.inputErrorClass);
+    errorElement.classList.remove(config.errorClass);
     errorElement.textContent = '';
 };
 
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add('pop-up__save_type_disabled');
+const toggleButtonState = (inputList, buttonElement, config) => {
+  if (hasInvalidInput(inputList, config)) {
+    buttonElement.classList.add(config.inactiveButtonClass);
+    buttonElement.disabled = true;
   } else {
-    buttonElement.classList.remove('pop-up__save_type_disabled');
+    buttonElement.classList.remove(config.inactiveButtonClass);
+    buttonElement.disabled = false;
   }
 }
 
@@ -201,34 +202,41 @@ const hasInvalidInput = (inputList) => {
   })
 };
 
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, config) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage,  config);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement,  config);
   }
 };
 
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.edit-form__field')); // проверь поля потом
-  const buttonElement = formElement.querySelector('.pop-up__save');
-    toggleButtonState(inputList, buttonElement);
-    inputList.forEach((inputElement) => {
+const setEventListeners = (formElement, config) => {
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector)); 
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+      inputList.forEach((inputElement) => {
       inputElement.addEventListener('input', function () {
-        checkInputValidity(formElement, inputElement);
-        toggleButtonState(inputList, buttonElement);
+        checkInputValidity(formElement, inputElement, config);
+        toggleButtonState(inputList, buttonElement, config);
       });
     });
 };
 
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.edit-form'));
+const enableValidation = (config) => {
+  const formList = Array.from(document.querySelectorAll(config.formSelector));
     formList.forEach((formElement) => {
       formElement.addEventListener('submit', (evt) => {
          evt.preventDefault();
          });
-      setEventListeners(formElement);
+      setEventListeners(formElement, config);
+     
     });
 }
-
-enableValidation();
+ 
+enableValidation({
+  formSelector: '.edit-form',
+  inputSelector: '.edit-form__field',
+  submitButtonSelector: '.pop-up__save',
+  inactiveButtonClass: 'pop-up__save_type_disabled',
+  inputErrorClass: '.edit-form__field_type_error',
+  errorClass: 'edit-form__input-error_active'
+});
