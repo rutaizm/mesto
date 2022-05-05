2fcb3427d2fd1ea4ddb336827aa3a3a61959382e
Tatyana Izmailova <rutaizm@yandex.ru>
2022-05-05 22:01:07 +0300

    webpack complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/pages/index.js',
     output: {
         filename: 'main.js',
-          path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: ''
     },
     plugins: [
           new HtmlWebpackPlugin({
           template: path.resolve(__dirname, 'src', 'index.html')
-        })
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
       ],
     mode: 'development',
+    devServer: {
+      static: path.resolve(__dirname, './dist'), 
+      compress: true, 
+      port: 8080, 
+      open: true 
+    },
     module:  {
         rules: [
+            {
+              test: /\.js$/,
+              use: 'babel-loader',
+              exclude: '/node_modules/'
+            },
             {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, {
                     loader: 'css-loader',
-                    options: {
-                      importLoaders: 1
-                    }
+                    options: {importLoaders: 1}
                   },
-                  'postcss-loader'
+                  'postcss-loader' 
                 ]
               },
             {
