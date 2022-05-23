fe663b4f562445871dbde143ed3a73f09fa1e8bd
Tatyana Izmailova <rutaizm@yandex.ru>
2022-05-23 10:04:53 +0300

    first review hotfix

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,21 +1,24 @@
 export class UserInfo {
-    constructor(userName, userJob) {
+    constructor(userName, userJob, userAvatar) {
         this._userName = userName;
         this._userJob = userJob;
+        this._userAvatar = userAvatar;
     }
 
-    getUserInfo() {
-        
+    getUserInfo() {        
         const userData = {
             name : this._userName.textContent,
             about : this._userJob.textContent,
         }
-
         return userData;
     }
 
     setUserInfo(userInputs) {
         this._userName.textContent = userInputs.name;
-        this._userJob.textContent = userInputs.about; 
+        this._userJob.textContent = userInputs.about;
+    }
+
+    setUserAvatar(data) {
+        this._userAvatar.src = data.avatar;
     }
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,6 +16,7 @@ export const nameInput = formProfilePopup.querySelector('.edit-form__field_type_
 export const jobInput = formProfilePopup.querySelector('.edit-form__field_type_info');
 export const name = document.querySelector('.profile__name');
 export const job = document.querySelector('.profile__info');
+export const avatar = document.querySelector('.profile__avatar');
 
 export const buttonAddPhoto = document.querySelector('.profile__add-photo-button'); 
 export const photoPopupForm = document.querySelector('.edit-form_type_photo');
@@ -25,5 +26,3 @@ export const photoList = document.querySelector('.elements__photoes');
 
 export const buttonAvatar = document.querySelector('.profile__avatar-button');
 export const avatarEditForm = document.querySelector('.edit-form[name=addAvatarForm]');
-
-export const avatar = document.querySelector('.profile__avatar');
